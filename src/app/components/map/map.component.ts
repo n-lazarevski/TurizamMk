4b397078a2d8d477b0778c18759a4f2717030d74
Nikola <nikola2000sk@gmail.com>
2023-03-09 13:10:06 +0100

    map is now interactive

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,4 +1,4 @@
-import {Component, HostListener} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-map',
@@ -7,11 +7,11 @@ import {Component, HostListener} from '@angular/core';
 })
 export class MapComponent {
 
+  image: string = 'Mapp.png'
   constructor() {
   }
 
-  @HostListener('document:mousemove', ['$event'])
-  onMouseMove(e: any) {
-    console.log(e);
+  changeMap(area: string){
+    this.image = `${area}.png`
   }
 }
