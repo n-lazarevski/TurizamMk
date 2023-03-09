14930edd4a4956f521a401c13a967518556b35b6
AleksandarGudov <alek.gudov@gmail.com>
2023-03-09 15:58:12 +0100

    added city skopje

--- src/app/domain/cities.ts
+++ src/app/domain/cities.ts
@@ -4,14 +4,32 @@ const cities = {
     attractions: [
       {
         ime: "Fortress of Skopje",
-        desc: "Commonly referred as Kale, is the highest point in the city overlooking the river Vardar, is dated 6th century."
+        desc: "Commonly referred as Kale, is the highest point in the city overlooking the river Vardar, is dated 6th century.",
+        image: "https://media.tacdn.com/media/attractions-splice-spp-674x446/0b/39/a2/c3.jpg"
       },
       {
         ime: "Canyon Matka",
-        desc: "Covering over 5,000 hectares, is one of the most popular outdoor destination in Macedonia, and is home to several medieval monasteries."
+        desc: "Covering over 5,000 hectares, is one of the most popular outdoor destination in Macedonia, and is home to several medieval monasteries.",
+        image: "https://www.mywanderlust.pl/wp-content/uploads/2020/06/matka-canyon-1.jpg"
       },
       {
-        ime: ""
+        ime: "Millennium Cross",
+        desc: "Built in 2002 is one of the tallest crosses worldwide towering at 66m tall, is situated on the top of Vodno Mountain.",
+        image: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0d/f8/c9/ca/photo0jpg.jpg?w=1200&h=-1&s=1"
+      }
+    ]
+  },
+  krusevo: {
+    ime: "Krusevo",
+    attractions:  [
+      {
+        ime: "Makedonium-Ilinden Monument",
+        desc: "Makedonium monument in Krusevo opened August 2 of 1974,  on the 30th anniversary of the Second Session of Anty-fascist Assembly for the National Liberation of Macedonia."
+        image: "https://encrypted-tbn1.gstatic.com/licensed-image?q=tbn:ANd9GcTLRQ3176Oqx9o4_vRqpHPH_PJkQBw9yQWbhyVbxmpy6BxA4JIF3aE97R4tvPs29g-cZSwyELt29gnkvp9dKbWzVQ"
+      },
+      {
+        ime: "Memorial House of Tose Proeski",
+        desc: ""
       }
     ]
   }
