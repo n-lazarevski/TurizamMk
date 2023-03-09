8a1c1b6b8460d340dad8af463c21c7ed46aaaddf
AleksandarGudov <alek.gudov@gmail.com>
2023-03-09 14:58:45 +0100

    changed dashboard

---
+++ src/app/domain/cities.ts
@@ -0,0 +1,18 @@
+const cities = {
+  skopje: {
+    ime: "Skopje",
+    attractions: [
+      {
+        ime: "Fortress of Skopje",
+        desc: "Commonly referred as Kale, is the highest point in the city overlooking the river Vardar, is dated 6th century."
+      },
+      {
+        ime: "Canyon Matka",
+        desc: "Covering over 5,000 hectares, is one of the most popular outdoor destination in Macedonia, and is home to several medieval monasteries."
+      }
+      {
+        ime: ""
+      }
+    ]
+  }
+}
